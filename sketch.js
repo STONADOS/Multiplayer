f43326ddb8f4008e8f4dada2a3586bfd0aaaa031
STONADOS <yogmahajan14345@gmail.com>
2020-09-13 18:48:24 +0530

    Multiplayer

--- sketch.js
+++ sketch.js
@@ -1,29 +1,43 @@
-var ball;
+var ball, database, position;
 
 function setup(){
+    database = firebase.database();
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+    ball = createSprite(250,250,50,50);
+    ball.shapeColor = "gold";
+    var ballposition = database.ref('ball/position');
+    ballposition.on("value", readPosition);
+
 }
 
 function draw(){
     background("white");
+    if(position != undefined){
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-5,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(5,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-5);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+5);
     }
     drawSprites();
 }
+}
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    database.ref('ball/position').set({
+        x:position.x + x,
+        y:position.y + y
+    });
+}
+
+function readPosition(data){
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
 }
